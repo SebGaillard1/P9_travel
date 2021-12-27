9b8f964ee87bb1815a2fa9b619e15f7cbddfa067
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2021-12-27 16:36:42 +0100

    Supported languages are now shown in the languagesTableView

--- P9_travel/Controller/LanguagesViewController.swift
+++ P9_travel/Controller/LanguagesViewController.swift
@@ -11,41 +11,58 @@ class LanguagesViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var languagesTableView: UITableView!
     
+    let cellId = "TableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         languagesTableView.dataSource = self
         languagesTableView.delegate = self
+        languagesTableView.register(UINib.init(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
         
         checkForLanguages()
     }
     
     private func checkForLanguages() {
         if TranslationManager.shared.supportedLanguages.isEmpty {
-            let ac = UIAlertController(title: "No languages", message: "There is no supported languages to translate to. Would you like to try to fetch languages now ?", preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "Fetch supported languages now", style: .default, handler: fetchSupportedLanguages(action:)))
-            ac.addAction(UIAlertAction(title: "Not now", style: .cancel, handler: { _ in
-                self.dismiss(animated: true)
-            }))
+//            let ac = UIAlertController(title: "No languages", message: "There is no supported languages to translate to. Would you like to try to fetch languages now ?", preferredStyle: .alert)
+//            ac.addAction(UIAlertAction(title: "Fetch supported languages now", style: .default, handler: fetchSupportedLanguages(action:)))
+//            ac.addAction(UIAlertAction(title: "Not now", style: .cancel, handler: { _ in
+//                self.dismiss(animated: true)
+//            }))
+            fetchSupportedLanguages()
         }
     }
     
-    private func fetchSupportedLanguages(action: UIAlertAction) {
-        // Show activity indicator
-        activityIndicator.isHidden = false
-        
-        // Try to fetch supported languages
+//    private func fetchSupportedLanguages(action: UIAlertAction) {
+//        // Show activity indicator
+//        activityIndicator.isHidden = false
+//
+//        // Try to fetch supported languages
+//        TranslationManager.shared.fetchSupportedLanguages { success in
+//            if success {
+//                // Display language in table view
+//                DispatchQueue.main.async {
+//                    self.languagesTableView.reloadData()
+//                }
+//            } else {
+//                // Inform : Failed to fetch languages
+//                let ac = UIAlertController(title: "Supported languages", message: "Something went wrong! Impossible to fetch supported languages", preferredStyle: .alert)
+//                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+//                    self.dismiss(animated: true)
+//                }))
+//            }
+//        }
+//    }
+    
+    private func fetchSupportedLanguages() {
         TranslationManager.shared.fetchSupportedLanguages { success in
             if success {
-                // Display language in table view
+                print("success")
                 DispatchQueue.main.async {
                     self.languagesTableView.reloadData()
                 }
             } else {
-                // Inform : Failed to fetch languages
-                let ac = UIAlertController(title: "Supported languages", message: "Something went wrong! Impossible to fetch supported languages", preferredStyle: .alert)
-                ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
-                    self.dismiss(animated: true)
-                }))
+                print("failed")
             }
         }
     }
@@ -57,7 +74,7 @@ extension LanguagesViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "languageCell", for: indexPath) as? LanguageCell else { return UITableViewCell() }
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? LanguageCell else { return UITableViewCell() }
                 
         let language = TranslationManager.shared.supportedLanguages[indexPath.row]
         
